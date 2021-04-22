b9b482ef4df40685b443d7d3e30ba1e0b89195a1
Michael Daigler <mdaigler2601@gmail.com>
2021-04-22 12:45:35 -0400

    added state vars :q q

--- Fundraising.sol
+++ Fundraising.sol
@@ -2,4 +2,20 @@ pragma solidity ^0.8.0;
 
 contract FundRaising {
 
+        mapping(address => uint) public contributors;
+        address public admin;
+        uint public numOfContributors;
+        uint public minContribution;
+        uint public deadline;
+        uint public goal;
+        uint raisedAmount = 0;
+        constructor(uint _goal, uint _deadline) {
+            admin = msg.sender;
+            numOfContributors = 0;
+            minContribution = 10;
+            deadline = block.timestamp + _deadline;
+            goal = _goal;
+        }
+
+
 }
